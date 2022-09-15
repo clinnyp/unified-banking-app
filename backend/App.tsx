9f3e880ea2d1bc106be6b0062a2a1f8f154518f1
Clinton Philathong <cphilathong1@gmail.com>
2022-09-15 19:17:46 +1200

    more ui features

--- backend/App.tsx
+++ backend/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -10,7 +10,27 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Accounts" component={Accounts} />
+        <Stack.Screen
+          name="Accounts"
+          component={Accounts}
+          options={{
+            // headerTitle: (props) => <LogoTitle {...props} />,
+            headerRight: () => (
+              <Button
+                onPress={() => console.log("logged out!!")}
+                title="Log Out"
+                color="#0A84FF"
+              />
+            ),
+            headerLeft: () => (
+              <Button
+                onPress={() => console.log("logged out!!")}
+                title="Transfer"
+                color="#0A84FF"
+              />
+            ),
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
