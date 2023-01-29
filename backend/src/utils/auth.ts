c653edc7d664c408a7e58282d91e14f945e27b6f
clintonphilathong <cphilathong1@gmail.com>
2023-01-30 04:03:11 +1300

    auth

---
+++ backend/src/utils/auth.ts
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+export const createJWT = (user) => {
+    const token = jwt.sign({});
+    return token;
+};
+
+export const protect = (req, res, next) => {
+    const bearer = req.headers.authorization;
+
+    if (!bearer) {
+        res.status(401);
+        res.json({ message: "not authorized" });
+        return;
+    }
+
+    const [, token] = bearer.split(" ");
+    if (!token) {
+        res.status(401);
+        res.json({ message: "not authorized" });
+        return;
+    }
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = user;
+        next();
+    } catch (e) {
+        res.status(401);
+        res.json({ message: "not authorized" });
+        return;
+    }
+};
+
+export const hashPassword = (password) => {
+    return bcrypt.hash(password, 12);
+};
+
+export const comparePassword = (password, hashedPassword) => {
+
+    return bcrypt.compare(password, hashPassword);
+};
