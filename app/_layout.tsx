75c72f79100523a2e1f589a14bde6093a7061eba
clinnyp <cphilathong1@gmail.com>
2023-12-07 00:17:19 +1300

    update stacks and tabs for file routing

---
+++ app/_layout.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack, Tabs } from 'expo-router'
+
+export default function RootLayout() {
+  return (
+    <RootLayoutNav  />
+  )
+}
+
+function RootLayoutNav() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+    </Stack>
+  )
+}
+
+const styles = StyleSheet.create({})
