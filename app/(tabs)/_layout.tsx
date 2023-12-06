22f117e19b0c007390fe8e20e4bedd6e011af074
clinnyp <cphilathong1@gmail.com>
2023-12-07 02:37:55 +1300

    update tab bar icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,43 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function TabLayout() {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
-        options={{ title: 'Accounts', headerTitleAlign: 'center' }}
+        options={{
+          headerRight: () => (
+            <Ionicons
+              name="person-circle"
+              style={{ marginRight: 15 }}
+              size={28}
+            />
+          ),
+          title: 'Accounts',
+          tabBarIcon: () => <Ionicons name="wallet" size={28} />,
+          headerTitleAlign: 'center',
+        }}
       />
       <Tabs.Screen
         name="payments"
-        options={{ title: 'Payments', headerTitleAlign: 'center' }}
+        options={{
+          title: 'Payments',
+          tabBarIcon: () => <Ionicons name="card" size={28} />,
+          headerTitleAlign: 'center',
+        }}
       />
       <Tabs.Screen
         name="transfer"
-        options={{ title: 'Transfer', headerTitleAlign: 'center' }}
+        options={{
+          title: 'Transfer',
+          tabBarIcon: () => (
+            <Ionicons name="paper-plane" size={27} color="black" />
+          ),
+          headerTitleAlign: 'center',
+        }}
       />
     </Tabs>
   )
