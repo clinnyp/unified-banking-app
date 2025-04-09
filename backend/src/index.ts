6ba2809014a0ff8858ac83961f40b0337de3c24d
clinnyp <cphilathong1@gmail.com>
2025-04-09 23:24:53 +1200

    add get accounts endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,28 @@
+import 'dotenv/config'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/accounts', async (c) => {
+  try {
+    const response = await fetch("https://api.akahu.io/v1/accounts", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.USER_TOKEN}`,
+        "X-Akahu-Id": `${process.env.APP_TOKEN}`
+      }
+    })
+    if (!response.ok) {
+      const code = response.status
+      return c.json({ success: false }, response.status as 400 | 401 | 403 | 404 | 500)
+    }
+    const data = await response.json()
+    return c.json({ success: true, data }, 200)
+  } catch (error) {
+    console.error(error)
+    return c.json({ success: false }, 500)
+  }
 })
 
 serve({
