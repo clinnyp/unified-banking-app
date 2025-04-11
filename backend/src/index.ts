a38ce9e293205d424331b53d02325ab290e56b6a
clinnyp <cphilathong1@gmail.com>
2025-04-12 02:46:57 +1200

    add transfer endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono().basePath("/api/v1")
 
 app.get('/accounts', async (c) => {
   try {
@@ -24,6 +24,35 @@ app.get('/accounts', async (c) => {
     return c.json({ success: false }, 500)
   }
 })
+
+app.post('/transfer', async (c) => {
+  try {
+    const data = await c.req.json()
+    console.log(data)
+    const response = await fetch("https://api.akahu.io/v1/transfers", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.USER_TOKEN}`,
+        "X-Akahu-Id": `${process.env.APP_TOKEN}`
+      }, body: JSON.stringify({
+        to: data.to._id,
+        from: data.from._id,
+        amount: data.amount
+      })
+    })
+
+    if (!response.ok) {
+      return c.json({ success: false }, response.status as 400 | 401 | 403 | 404 | 500)
+    }
+
+    return c.json({ success: true, data }, 200)
+  } catch (error) {
+    console.error(error)
+    return c.json({ success: false }, 500)
+  }
+})
+
 
 serve({
   fetch: app.fetch,
